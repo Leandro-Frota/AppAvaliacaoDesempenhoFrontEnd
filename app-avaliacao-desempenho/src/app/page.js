20fed40470c32db9763f6addf77906b09b8e109e
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2024-08-13 23:23:19 -0300

    Add text Introduction componets and alt styles

--- app-avaliacao-desempenho/src/app/page.js
+++ app-avaliacao-desempenho/src/app/page.js
@@ -6,7 +6,17 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center p-10">
       {/* <Register/> */}
-      INTRODUÇÃO
+      <p>
+
+A avaliação de desempenho de funcionários é uma ferramenta essencial nas empresas para medir a eficiência,
+produtividade e alinhamento dos colaboradores com os objetivos organizacionais. Esse processo permite identificar
+ pontos fortes, áreas de melhoria e definir planos de desenvolvimento personalizados. <br></br><br></br>
+
+Na metodologia descrita, cada quesito avaliado recebe uma pontuação específica: a nota A,
+correspondente a um desempenho excelente, vale 7 pontos; a nota B, indicando um desempenho satisfatório,
+vale 4 pontos; e a nota C, que reflete um desempenho abaixo do esperado, vale 1 ponto. Essas pontuações
+permitem quantificar o desempenho de cada funcionário de forma objetiva, facilitando a comparação e análise dos resultados.
+      </p>
     </main>
   );
 }
--- app-avaliacao-desempenho/src/app/register/page.js
+++ app-avaliacao-desempenho/src/app/register/page.js
@@ -2,11 +2,11 @@ import InputLabel from "@/components/InputLabel/InputLabel";
 
 export default function Register(){
     return (
-        <div className="w-3/6">
-            <div className="text-start text-xl">
+        <div className="w-3/6  p-10">
+            <div className="text-start text-xl ">
                 <h2>Cadastro de Funcionário</h2>
             </div>
-            <div className="flex flex-col  gap-2 w-3/6 ">
+            <div className="flex flex-col  gap-2   ">
                 <InputLabel label="Nome" type="text"/>
                 <InputLabel label="Matrícula" type="number"/>
                 <InputLabel label="Função" type="text"/>
